c518bcf40c33949ed763a955354cfb35cdea4d3f
deadoggy <deadoggy@aliyun.com>
2017-05-24 09:11:03 +0800

    还差页面交互的内容

--- src/main/java/com/deliver/controller/HomeController.java
+++ src/main/java/com/deliver/controller/HomeController.java
@@ -8,6 +8,7 @@ import com.deliver.dao.PackageRepository;
 import com.deliver.model.Package;
 import com.deliver.service.CAPTCHAService;
 import com.deliver.service.ShortMesgService;
+import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -31,9 +32,60 @@ public class HomeController {
     public String login(){
         return "login";
     }
+
     @RequestMapping({"/home"})
-    public String showHomePage(){
+    public String homePage(){
         return "manage_cup";
+    }
+
+    @RequestMapping({"/cupboard"})
+    public String manageCupboardPage(){
+        return "manage_cup";
+    }
+
+    @RequestMapping({"/taken_record"})
+    public String takenRecordPage(){
+        return "taken_record";
+    }
+
+    @RequestMapping({"/send_package"})
+    public String sendPackagePage(){
+        return "send_package";
+    }
+
+    @RequestMapping({"/store_package"})
+    public String storePackagePage(){
+        return "store_package";
+    }
+
+    @RequestMapping({"/shelf_cup_transfer"})
+    public String shelfCupTransferPage(){
+        return "shelf_cup_transfer";
+    }
+
+    @RequestMapping({"/generate_form"})
+    public String generateFormPage(){
+        return "generate_form";
+    }
+
+    @RequestMapping({"/account_manage"})
+    public String accountManagePage(){
+        return "account_manage";
+    }
+
+    @RequestMapping({"/search_package"})
+    public String searchPackagePage(){
+        return "search_package";
+    }
+
+    @RequestMapping({"/taken_out_time"})
+    public String takenOutTimePage(){
+        return "taken_out_time";
+    }
+
+    @RequestMapping({"/advanced_manage"})
+    public String advancedManagePage(){
+        return "advanced_manage";
     }
 
 

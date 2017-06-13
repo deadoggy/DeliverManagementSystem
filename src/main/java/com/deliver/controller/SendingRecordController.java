c050c97d37fdccd1247751cef0788ca73a45807f
flyboss <915741005@qq.com>
2017-06-13 20:58:29 +0800

    添加了寄件记录的all查询

--- src/main/java/com/deliver/controller/SendingRecordController.java
+++ src/main/java/com/deliver/controller/SendingRecordController.java
@@ -95,4 +95,40 @@ public class SendingRecordController {
             return jsonObject.toJSONString();
         }
     }
+
+    @RequestMapping(value = "/sendrecord",method = RequestMethod.GET)
+    public String getAll(){
+        try{
+            List<SendingRecord> sendingRecordList=sendingService.getAll();
+            if(sendingRecordList==null){
+                return "{\"status\": \"fail\", \"reason\" : \"请求失败\"}";
+            }else{
+                JSONObject jsonObject=new JSONObject();
+                jsonObject.put("status", "ok");
+                JSONArray jsonArray = new JSONArray();
+                for (SendingRecord sendingRecord:sendingRecordList) {
+                    JSONObject sendingRecordObject = new JSONObject();
+                    sendingRecordObject.put("id", sendingRecord.getmSending_record_id());
+                    sendingRecordObject.put("sName",sendingRecord.getmSenderName());
+                    sendingRecordObject.put("sAddress",sendingRecord.getmSenderAddress());
+                    sendingRecordObject.put("sCity",sendingRecord.getmSenderCity());
+                    sendingRecordObject.put("sProvince",sendingRecord.getmSenderProvince());
+                    sendingRecordObject.put("sTele",sendingRecord.getmSenderTele());
+                    sendingRecordObject.put("sTime",sendingRecord.getmSendTime());
+                    sendingRecordObject.put("rName",sendingRecord.getmReceiverName());
+                    sendingRecordObject.put("rAddress",sendingRecord.getmReceiverAddress());
+                    sendingRecordObject.put("rCity",sendingRecord.getmReceiverCity());
+                    sendingRecordObject.put("rProvince",sendingRecord.getmReceiverProvince());
+                    sendingRecordObject.put("rTele",sendingRecord.getmReceiverTele());
+                    sendingRecordObject.put("weight",sendingRecord.getmWeight());
+                    jsonArray.add(sendingRecordObject);
+                }
+                jsonObject.put("sendingRecordList", jsonArray);
+                return jsonObject.toJSONString();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return "{\"status\": \"fail\", \"reason\" : \"请求失败\"}";
+        }
+    }
 }
